6777f99907568fb18ecb03876175ad3bf6bff025
Brice Francois <ngahanb@gmail.com>
2020-07-29 14:10:04 +0200

    Donald Kapyouo

--- computer.py
+++ computer.py
@@ -1,38 +1,46 @@
-import serial
+#Serial ist das Modul, das uns ermöglicht mit dem Pyboard zu kommunizieren
+import serial 
 
+#wir greifen dem Pfad von dem Pyboard zu
 ser = serial.Serial("/dev/tty.usbmodem3571314C30372", 115200, timeout=3)
 
-channelX3 = []
-channelY7 = []
+channelX3 = [] #Feld für die Ersten Messungen
+channelY7 = [] #Feld dür die Zweiten Messungen
 
-
+#Wartezeit damit, sich dem Komputer mit dem Pyboard verbindet
 while ser.in_waiting==0:
     print("waiting for pyboard...")
+
+
+#Unendliche Schleife, damit dem Komputer ständig mit dem Pyboard im Verbindung steht und dass mit einem Break abgebrochen wird 
 while True:
     #ser.write(b"WeitereMessungen \r\n")
-    recv = ser.readline()
-    recv = str(recv)
-    recvs = recv.replace("\\r\\n'","")
-    recvs = recvs.replace("\\r\\n","")
-    
-        
-    if recvs[0:4]== "b'X3":
-        recv_ = recvs.replace("b'X3", "")
-        recvInt = int(recv_)
-        channelX3.append(recvInt)
-    elif recvs[0:4] == "b'Y7":
-        recv_ = recvs.replace("b'Y7", "")
-        recvInt = int(recv_)
-        channelY7.append(recvInt)
-    elif recvs == "b'ShowMeasurements":
+    recv = ser.readline()                     #empfang die Daten
+    recv = str(recv)                          #umwandelt die Daten in String 
+    recvs = recv.replace("\\r\\n'","")        #Bearbeitung der Daten
+    recvs = recvs.replace("\\r\\n","")        #weitere Bearbeitung
+
+
+#Trennung der empfangenen Daten 
+    if recvs[0:4]== "b'X3":                   #Daten von den ersten Messungen
+        recv_ = recvs.replace("b'X3", "")     #Bearbeitung
+        recvInt = int(recv_)                  #umwandlung in Integer
+        channelX3.append(recvInt)             #füge im ersten Feld hinzu
+
+    elif recvs[0:4] == "b'Y7":                #Daten von den ersten Messungen
+        recv_ = recvs.replace("b'Y7", "")     #Bearbeitung
+        recvInt = int(recv_)                  #umwandlung in Integer
+        channelY7.append(recvInt)             #füge im ersten Feld hinzu
+
+    elif recvs == "b'ShowMeasurements":       #Zeige anschließend die Messungen
         print("measurements von Pin X3: ")
-        print(channelX3)
+        print(channelX3)                      #erste Messungen
         print("measurements von Pin Y7: ")
-        print(channelY7)
+        print(channelY7)                      #zweiten Messungen
     elif recvs == "b'endeMessungen":
-        break
+        break                                 #end
     else:
-        print(recvs)
+        print(recvs)                          #wenn nichts passiert dann ist vermutlich einen Fehler aufgetreten... wir zeigen die Meldung mit print
 
     ser.write(b'zeigeMeasurments\r\n')
     
--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# main.py -- put your code here
-import uasyncio
-import uarray
-import pyb
+import uasyncio #uasyncio ermöglicht eine gleichzeitige bearbeitung der Messungen und die Daten zu stören
+import uarray   #uarray ist das Feld in Micropython
+import pyb      #pyb ist das Modul des Pyboards die das Zugreifen der verschiedenen Pins des Pyboards ermöglich
 
-locker = uasyncio.Lock()
-total_measurements = 20
-schon_gemessen = 0
+locker = uasyncio.Lock() #schließt und öffnet Teilprogrammen
+total_measurements = 20  #Anzahl der gesamten Messungen
+schon_gemessen = 0       #Anzahl der schon gemessene Messungen
 
-
-async def empfanger(port):
+ async def empfanger(port):   #Verbindung mit dem Komputer
     global total_measurements
     global schon_gemessen
-    swriter = uasyncio.StreamWriter(port)
+    swriter = uasyncio.StreamWriter(port) 
     sreader = uasyncio.StreamReader(port)
+
+    #Unendliche Schleife, damit dem Komputer ständig mit dem Pyboard im Verbindung steht und dass mit einem Break abgebrochen wird 
     while True:
-        await locker.acquire()
-        befehl = await sreader.readline()
-        befehl_ = str(befehl).replace("\\r\\n'", "")
+        await locker.acquire() #das Einzige Teilprogramm das läuft die anderen Teilprogrammen sind gesperrt 
+        befehl = await sreader.readline() #empfangt ein Befehl von dem Komputer 
+        befehl_ = str(befehl).replace("\\r\\n'", "") #Bearbeitung des Befehls
         
         if befehl_ == "b'zeigeMeasurments":
-            await swriter.awrite("ShowMeasurements\r\n")
+            await swriter.awrite("ShowMeasurements\r\n") #send dem Komputer ein Befehl, um ihn die förden, die gemessenen Daten zu zeigen
         
         await swriter.awrite("end\r\n")
 
         if schon_gemessen == total_measurements :
-            await swriter.awrite("endeMessungen\r\n")
+            await swriter.awrite("endeMessungen\r\n") #send dem Komputer ein Befehl, um ihn die förden, das Programm zu beenden
             break
-        locker.release()
+        locker.release() #ermöglicht den anderen Teilprogrammen weiter zu laufen
         await uasyncio.sleep(1)        
 
 
-
+#Empfang der Messungen des Sensors bzw Potentiometers
 async def start_measurment(pin, port, Anzahl_measurments, timeout):
     global total_measurements
     global schon_gemessen
@@ -39,29 +39,30 @@ async def start_measurment(pin, port, Anzahl_measurments, timeout):
     adc = pyb.ADC(pyb.Pin(pin))
 
     for i in range(Anzahl_measurments):
-        await locker.acquire()
-        adc_ = adc.read()
-        adcstr = pin+" "+str(adc_)+ "\r\n"
-        await usbwriter.awrite(adcstr)
+        await locker.acquire() #das Einzige Teilprogramm das läuft die anderen Teilprogrammen sind gesperrt 
+        adc_ = adc.read() #Empfang der Messungen
+        adcstr = pin+" "+str(adc_)+ "\r\n" #Bearbeitung der Messungen
+        await usbwriter.awrite(adcstr)     #Sendung der Messungen an dem Komputer
         schon_gemessen += 1
-        locker.release()
+        locker.release() #ermöglicht den anderen Teilprogrammen weiter zu laufen
         await uasyncio.sleep(timeout/Anzahl_measurments)   
-        
+
+#Haupt Function        
 async def main():
     usb = pyb.USB_VCP()
-    pyb.Pin("EN_3V3").on()
+    pyb.Pin("EN_3V3").on() #Aktivation der Spannungsquelle 
     
 
-    task_ =  uasyncio.create_task(start_measurment("X3",usb,15, 10))
-    task__ = uasyncio.create_task(start_measurment("Y7",usb,5, 5))
-    befehl = uasyncio.create_task(empfanger(usb))
+    task_ =  uasyncio.create_task(start_measurment("X3",usb,15, 10)) #Daten des ersten Pins
+    task__ = uasyncio.create_task(start_measurment("Y7",usb,5, 5))   #Daten des zweiten Pins 
+    befehl = uasyncio.create_task(empfanger(usb)) #Kommunikation zwischen Pyboard und Komputer
 
-    print("Thread principal démaré")
+    print("Starten Haupt Threads")
     await task_
     await task__
     await befehl
     
     empfanger(usb)
-    print("fin du programme")
+    print("Ende des Programm")
 
 uasyncio.run(main())
